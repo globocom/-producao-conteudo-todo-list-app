8db07e61690e804069c036e7e12822e37b11c4dc
Felipe Rosas <contato@eufelipe.com>
2024-04-08 19:42:32 -0300

    chore: add styles utils

---
+++ src/utils/stylesUtils.ts
@@ -0,0 +1,18 @@
+import { Dimensions, StyleProp, ViewStyle } from "react-native";
+
+const LARGE_DEVICE_WIDTH = 768;
+
+export const getContainerStyles = (
+  widthPercentage: number = 0.5
+): StyleProp<ViewStyle> => {
+  const windowWidth = Dimensions.get("window").width;
+
+  if (windowWidth > LARGE_DEVICE_WIDTH) {
+    return {
+      width: `${widthPercentage * 100}%`,
+      alignSelf: "center",
+    };
+  }
+
+  return {};
+};
