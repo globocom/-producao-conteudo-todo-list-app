8fd466d652eafd165f34880e5f87806a28399bd0
Felipe Rosas <contato@eufelipe.com>
2024-04-08 19:43:42 -0300

    chore: update main

---
+++ src/Main.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet, View } from "react-native";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+import { useTask } from "./hooks/useTask";
+import { getContainerStyles } from "./utils/stylesUtils";
+
+export default function Main() {
+  const { tasks, addTask, toggleTask, deleteTask } = useTask();
+
+  return (
+    <View style={[styles.container, getContainerStyles()]}>
+      <TaskForm addTask={addTask} />
+      <TaskList tasks={tasks} toggleTask={toggleTask} deleteTask={deleteTask} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    paddingTop: 50,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    margin: 20,
+  },
+});
