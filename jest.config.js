bb21267013db98183f54359218e7c82fe461f7c6
Felipe Rosas <felipe.rosas@g.globo>
2025-01-22 15:39:00 -0300

    chore: fix jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   preset: "jest-expo",
-  transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
-  ],
+  testEnvironment: "jsdom",
   setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
 };
